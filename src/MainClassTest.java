55b301301afdb14b71aaf595be87394904a396bd
Татьяна Лозовская <tanya-lozovskaya@yandex.ru>
2025-04-04 00:43:33 +0700

    Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 
 public class MainClassTest {
     @Test
-    public void testGetClassNumber() {
+    public void testGetClassString() {
         MainClass mainClass = new MainClass();
 
-        int actualResult = mainClass.getClassNumber();
+        Object actualResult = mainClass.getClassString();
 
-        Assert.assertTrue("Метод getClassNumber должен возвращать число больше 45", actualResult > 45);
+        Assert.assertTrue("Возвращаемое значение должно быть строкой", actualResult instanceof String);
+
+        String strResult = (String) actualResult;
+
+        boolean containsHello = strResult.contains("Hello") || strResult.contains("hello");
+
+        Assert.assertTrue("Строка должна содержать Hello или hello", containsHello);
     }
 }
