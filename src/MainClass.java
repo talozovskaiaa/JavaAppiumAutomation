88be715df3edd30fddd45d5e3488cfd279c112d4
Татьяна Лозовская <tanya-lozovskaya@yandex.ru>
2025-04-03 22:19:14 +0700

    Первое домашнее задание

---
+++ src/MainClass.java
@@ -0,0 +1,8 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClass {
+    public int getLocalNumber() {
+        return 14;
+    }
+}
---
+++ src/MainClassTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest {
+    @Test
+    public void testGetLocalNumber() {
+        MainClass mainClass = new MainClass();
+
+        int actualResult = mainClass.getLocalNumber();
+        int expectedResult = 14;
+
+        Assert.assertTrue("Метод getLocalNumber должен возвращать число 14", actualResult == expectedResult);
+    }
+}
