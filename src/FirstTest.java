c6176a542217fd798a0dde22e2bdac1b94cdd71a
Татьяна Лозовская <tanya-lozovskaya@yandex.ru>
2025-04-19 02:17:02 +0700

    Лекция 3, ДЗ 1

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,15 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.net.URL;
 
 public class FirstTest {
@@ -32,9 +38,165 @@ public class FirstTest {
             driver.quit();
         }
 
+//        @Test
+//        public void FirstTest()
+//        {
+//            waitForElementAndClick(
+//                    By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                    "Cannot find Search Wikipedia input",
+//                    5
+//            );
+//
+//            waitForElementAndSendKeys(
+//                    By.xpath("//*[contains(@text,'Search…')]"),
+//                    "Java",
+//                    "Cannot find search input",
+//                    5
+//            );
+//
+//            waitForElementPresent(
+//                    By.xpath("//*[contains(@resource-id,'org.wikipedia:id/page_list_item_container')]//*[contains(@text,'Object-oriented programming language')]"),
+//                    "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
+//                    15
+//            );
+//        }
+//
+//        @Test
+//        public void testCancelSearch()
+//        {
+//            waitForElementAndClick(
+//                    By.id("org.wikipedia:id/search_container"),
+//                    "Cannot find 'Search Wikipedia' input",
+//                    5
+//            );
+//
+//            waitForElementAndSendKeys(
+//                    By.xpath("//*[contains(@text,'Search…')]"),
+//                    "Java",
+//                    "Cannot find search input",
+//                    5
+//            );
+//
+//            waitForElementAndClear(
+//                    By.id("org.wikipedia:id/search_src_text"),
+//                    "Cannot find search clear",
+//                    5
+//            );
+//
+//            waitForElementAndClick(
+//                    By.id("org.wikipedia:id/search_close_btn"),
+//                    "Cannot find X to cancel search",
+//                    5
+//            );
+//
+//            waitForElementNotPresent(
+//                    By.id("org.wikipedia:id/search_close_btn"),
+//                    "X is still present on the page",
+//                    5
+//            );
+//        }
+//
+//        @Test
+//        public void testCompareArticelTitle()
+//        {
+//            waitForElementAndClick(
+//                    By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                    "Cannot find Search Wikipedia input",
+//                    5
+//            );
+//
+//            waitForElementAndSendKeys(
+//                    By.xpath("//*[contains(@text,'Search…')]"),
+//                    "Java",
+//                    "Cannot find search input",
+//                    5
+//            );
+//
+//            waitForElementAndClick(
+//                    By.xpath("//*[contains(@resource-id,'org.wikipedia:id/page_list_item_container')]//*[contains(@text,'Object-oriented programming language')]"),
+//                    "Cannot find 'Search Wikipedia' input",
+//                    5
+//            );
+//
+//            WebElement title_element = waitForElementPresent(
+//                    By.id("org.wikipedia:id/view_wiki_error_text"),
+//                    "Cannot find acticle title",
+//                    15
+//            );
+//
+//            String acticle_title = title_element.getAttribute("text");
+//
+//            Assert.assertEquals(
+//                    "We see unexpected title",
+//                    "An error occurred",
+//                    acticle_title
+//            );
+//        }
+//
+// Тема 3, ДЗ 1
         @Test
-        public void FirstTest()
+        public void article_has_test()
         {
-            System.out.println("First test run");
+            waitForElementAndClick(
+                    By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                    "Cannot find Search Wikipedia input",
+                    5
+            );
+
+            assertElementHasText(
+                    By.xpath("//*[contains(@text,'Search…')]"),
+                    "",
+                    "Cannot find search input"
+            );
         }
-    }
+
+        private WebElement waitForElementPresent(By by, String error_message, long timeoutIntSeconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, timeoutIntSeconds);
+            wait.withMessage(error_message + "\n");
+            return wait.until(
+                    ExpectedConditions.presenceOfElementLocated(by)
+            );
+        }
+        private WebElement waitForElementPresent(By by, String error_message)
+        {
+        return waitForElementPresent(by, error_message, 5);
+        }
+
+        private WebElement waitForElementAndClick(By by, String error_message, long timeOutInSecond)
+        {
+          WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
+          element.click();
+          return element;
+        }
+
+//        private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeOutInSecond)
+//        {
+//            WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
+//            element.sendKeys(value);
+//            return element;
+//        }
+
+//         private boolean waitForElementNotPresent(By by, String error_message, long timeOutInSecond)
+//        {
+//            WebDriverWait wait = new WebDriverWait(driver, timeOutInSecond);
+//            wait.withMessage(error_message + "\n");
+//            return wait.until(
+//                ExpectedConditions.invisibilityOfElementLocated(by)
+//            );
+//        }
+
+//        private WebElement waitForElementAndClear(By by, String error_message, long timeOutInSecond)
+//        {
+//            WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
+//            element.clear();
+//            return element;
+//        }
+// Тема 3, ДЗ 1
+        private WebElement assertElementHasText(By by, String value, String error_message)
+        {
+            WebElement element = waitForElementPresent(by, error_message);
+            element.sendKeys(value);
+            return element;
+        }
+}
---
+++ src/com/itproger/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public void main(String[] args) {
+        int a = 15, b = 10;
+        if(a > b) {
+            System.out.println("верно");
+            }
+}
